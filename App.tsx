0f2cfb3757c8b283ee6cec62a8d36e1be84ceac7
jatsala <jatsalaberria@gmail.com>
2024-06-04 08:18:16 -0400

    Advent calendar list

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import DayListItem from './src/components/core/DayListItem';
+
+const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world</Text>
-      <Text>DEVember</Text>
+      <FlatList
+        data={days}
+        contentContainerStyle={styles.content}
+        columnWrapperStyle={styles.column}
+        numColumns={2}
+        renderItem={({ item }) => <DayListItem day={item} />}
+      />
+
+      {/* {days.map((day, id) => (
+        <View style={styles.box} key={id}>
+          <Text style={styles.text}>{day}</Text>
+        </View>
+      ))} */}
+
       <StatusBar style='auto' />
     </View>
   );
@@ -15,7 +30,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  content: {
+    gap: 10,
+    padding: 10,
+  },
+  column: {
+    gap: 10,
   },
 });
