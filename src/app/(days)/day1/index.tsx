df9698b6a66636b284124c93bf09be6b89505a16
jatsala <jatsalaberria@gmail.com>
2024-06-05 09:53:40 -0400

    stack navigator

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,9 +1,11 @@
+import { Stack } from 'expo-router';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const DayDetailsScreen = () => {
   return (
     <View>
+      <Stack.Screen options={{ title: 'Day 1' }} />
       <Text>Day Details Screen</Text>
     </View>
   );
