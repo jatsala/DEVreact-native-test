0b205538039407b2625f50bfc9e288ccfd0895df
jatsala <jatsalaberria@gmail.com>
2024-06-07 19:23:03 -0400

    Makdown-display

--- src/app/(days)/day3/index.tsx
+++ src/app/(days)/day3/index.tsx
@@ -1,17 +1,34 @@
-import { Link, Stack } from 'expo-router';
-import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Link, Stack } from 'expo-router';
+import MarkdownDisplay from '@/components/day3/MarkdownDisplay';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+const description = `
+# Markdown
+
+Integrate Markdown content in **React Native**
+
+📚 Today's Agenda:
+- Introduction to Markdown
+- Markdown Syntax Overview
+- Setting Up React Native for Markdown
+- Implementing Markdown Rendering
+- Styling Markdown Content
+- Using Markdown in React Native Components
+- Recap and Q&A Session
+`;
 
 const DayDetailsScreen = () => {
   return (
-    <View>
+    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
       <Stack.Screen options={{ title: 'Day 3: Markdown' }} />
-      <Text>Day Details Screen</Text>
+      <MarkdownDisplay>{description}</MarkdownDisplay>
 
       <Link href='/day3/editor' asChild>
         <Button title='Go to editor' />
       </Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
